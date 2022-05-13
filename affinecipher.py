e8d9a6e6337b0dadae25139a370e688368763cf5
WindOfXaos <mouaz30970893@f-eng.tanta.edu.eg>
2022-05-13 22:59:59 +0200

    first commit

--- affinecipher.py
+++ affinecipher.py
@@ -1,16 +1,20 @@
-
 import sys
 import string
-import platform
 
-calcenc = lambda c, a, b: (a * c + b) % 26
-calcdec = lambda c, a, b: (keyinverse(a, 26) * (c - b)) % 26
 
+lower = string.ascii_lowercase # lowercase letters
+upper = string.ascii_uppercase # uppercase letters
+m = len(lower)
+
+calcenc = lambda c, a, b: (a * c + b) % m # encryption function for a single letter
+calcdec = lambda c, a, b: (keyinverse(a, m) * (c - b)) % m # decryption function for a single letter
+
+# calculates the modular multiplicative inverse a^-1
 def keyinverse(a, m) :
-    for i in range(1, m) :
-        if (((a % m) * (i % m)) % m == 1) :
+    for i in range(m) :
+        if (a * i % m == 1) :
             return i
-    return -1 ;
+    return -1;
 
 def encryption(p, a, b) :
     c = ""
@@ -36,19 +40,28 @@ def decryption(c, a, b) :
             p += i
     return p
 
+def gcd(a, b):
+    # Everything divides 0
+    if (a == 0 or b == 0): return 0
+    # base case
+    if (a == b): return a
+    # a is greater
+    if (a > b):
+        return gcd(a - b, b)
+    # b is greater
+    return gcd(a, b - a)
+
+def isCoprime(a, b):
+    if (gcd(a, b) == 1):
+        return True;
+    else:
+        return False;
+
 if __name__ == "__main__" :
-
-    lower = string.ascii_lowercase
-    upper = string.ascii_uppercase  
-
-    if platform.system().startswith("Linux") :
-	    red , green , yellow , blue , endc = '\033[91m' , '\033[92m' ,'\033[93m' , '\033[94m' , '\033[0m'
-    else :
-	    red = green = yellow = blue = endc = ""
 
     if len(sys.argv) < 5 :
 
-        print(red + "! Usage: " + endc + "python3 affinecipher.py <type> <string> <a> <b>")
+        print("Usage: python3 affinecipher.py <type> <string> <a> <b>")
         print('''
         - type    : {enc: encryption, dec: decryption}
         - string  : the text you want to enrypt or decrypt
@@ -56,7 +69,7 @@ if __name__ == "__main__" :
         - b       : the second operand of the key
     ''')
 
-        print(yellow + "* Note: " + endc + "make sure you add double quotes in case the string has whitespaces")
+        print("Note: make sure you add double quotes in case the string has whitespaces")
         exit()
     
     args= [i for i in sys.argv]
@@ -65,12 +78,15 @@ if __name__ == "__main__" :
         b = int(args[4])
     
     except :
-        print(red + 'Error: ' + endc + 'Operands must be numbers not strings')
+        print('Error: Operands must be numbers not strings')
         exit()
+
+    if not isCoprime(a, m):
+        print('Warning: Operand "a = ' + str(a) + '" is not coprime with "m = ' + str(m) + '"')
 
     if args[1] == "enc" :
         print(encryption(args[2], a, b))
     elif args[1] == "dec" :
         print(decryption(args[2], a, b))
     else :
-        print(red + "!Error: " + endc + "Unvalid type of operation")
+        print("Error: Unvalid type of operation")
